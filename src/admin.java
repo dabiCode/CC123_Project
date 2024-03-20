3df9d93bdfe71bbb1a24cc881b96a5a234c17431
brylle60 <johnbryllecrodua77@gmail.com>
2024-03-20 17:41:12 +0800

    src

---
+++ src/admin.java
@@ -0,0 +1,15 @@
+public class admin {
+    int user;
+    String account_name;
+    double bill;
+    String admin_acc;
+   admin_acc = user(user, bill);
+   
+
+  public int user(int deposit, int bill)
+  {
+        int user;
+        String account_name;
+  }
+
+}
