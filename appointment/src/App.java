e490af58152b43f93565b377e989848e820f4395
brylle60 <johnbryllecrodua77@gmail.com>
2024-04-04 20:26:38 +0800

    db

--- appointment/src/App.java
+++ appointment/src/App.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) {
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/apointment", "root", "brylle1234");
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/appointment", "root", "brylle1234");
              Statement stmt = con.createStatement();
              ResultSet rs = stmt.executeQuery("SELECT * FROM app")) {
 
