9f4f7b2261280c437639490d4bdb304ff85bd8e8
brylle60 <johnbryllecrodua77@gmail.com>
2024-04-09 15:27:38 +0800

    new updated branch

---
+++ appoinment/src/gui/launcher.java
@@ -0,0 +1,25 @@
+package gui;
+
+import db.MyJDBC;
+import db.userDb;
+
+import javax.swing.*;
+
+public class launcher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new loginpage().setVisible(true);
+//new menu().setVisible(true);
+              // System.out.println(MyJDBC.checkuser("username1234"));
+               //System.out.println(MyJDBC.register("username1234", "password"));
+               // System.out.println(MyJDBC.validatelogin("username1234", "password"));
+               //System.out.println(userDb.checkuser(Integer.parseInt("1234")));
+               // System.out.println(userDb.validateuser(Integer.parseInt("123"), "admin", "admin","admin", "4:20:30"));
+                //System.out.println(userDb.menu(Integer.parseInt("123"), "admin", "admin","admin", "4:20:30"));
+
+            }
+        });
+    }
+}
