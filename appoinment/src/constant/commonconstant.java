6bb60bc708f4121b2ffa9c2720af668dd84eadc2
P-Chel <chelsiefaith07@gmail.com>
2024-04-23 21:11:36 +0800

    new

--- appoinment/src/constant/commonconstant.java
+++ appoinment/src/constant/commonconstant.java
@@ -16,7 +16,13 @@ public class commonconstant {
     public static final Color BLUE_COLOR = Color.decode("#FFFFFF");
     //pastel blue
     public static final Color BUTTON_COLOR = Color.decode("#ADD8E6");
-//
+
+    //gray-ish home page
+    public static final Color HOME_BG1_GRAY = Color.decode("#e5f5ee");
+    //blue dark home page
+    public static final Color HOME_BG1_BLUE = Color.decode("#017ac2");
+
+    public static final Color HOME_BG1_GRAYISH = Color.decode("#aec6cf");
 
     // mySQL server connector
     public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/user_schema";
