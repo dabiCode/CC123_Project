83fc39b78dd3e4dbf9bb376f6c0bdabfc735b132
KentRomer <krmadera01@gmail.com>
2024-04-24 17:53:58 +0800

    new about

--- appoinment/src/gui/About.java
+++ appoinment/src/gui/About.java
@@ -4,6 +4,8 @@ import constant.commonconstant;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class About extends homepage{
     public About(){
@@ -13,10 +15,17 @@ public class About extends homepage{
     }
 
     private void addGuiComponents() {
-        JLabel menulabel = new JLabel("HealthAppointment");
+        ImageIcon logoIcon = new ImageIcon("appoinment/src/image/20240424_162915_0000.png"); // Replace "path_to_your_logo_image_file.jpg" with the actual path to your image file
 
-        menulabel.setBounds(0, 20, 400, 100);
-        menulabel.setForeground(commonconstant.PRIMARY_COLOR.brighter());
+        // Create a JLabel to display the logo image
+        JLabel logoLabel = new JLabel(logoIcon);
+        logoLabel.setBounds(490, -30, 1000, 1000); // Adjust the position and size as needed
+        add(logoLabel);
+
+        JLabel menulabel = new JLabel("Health Appointment");
+
+        menulabel.setBounds(50, 70, 400, 100);
+        menulabel.setForeground(commonconstant.TEXT_COLOR.brighter());
 
         menulabel.setFont(new Font("Dialog", Font.BOLD, 20));
 
@@ -33,35 +42,72 @@ public class About extends homepage{
 
         home.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         home.setForeground(commonconstant.TEXT_COLOR);
-        home.setBounds(320, 150, 100, 25);
+
+
+        home.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                About.this.dispose();
+
+                new home().setVisible(true);
+            }
+        });
+
+        home.setBounds(950, 70, 100, 25);
         //reserved space for database
 
-        JButton features = new JButton("Features");
-        features.setFont(new Font("Dialog", Font.BOLD, 18));
-
-        features.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        features.setForeground(commonconstant.TEXT_COLOR);
-        features.setBounds(400, 150, 150, 25);
-
-        JButton about = new JButton("About Us");
-        about.setFont(new Font("Dialog", Font.BOLD, 18));
-
-        about.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        about.setForeground(commonconstant.TEXT_COLOR);
-        about.setBounds(550, 150, 160, 25);
-
-        JButton signin = new JButton("Logout");
-        signin.setFont(new Font("Dialog", Font.BOLD, 18));
-
-        signin.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        signin.setForeground(commonconstant.TEXT_COLOR);
-        signin.setBackground(commonconstant.BUTTON_COLOR);
-        signin.setBounds(680, 150, 150, 25);
 
 
-        add(signin);
-        add(about);
-        add(features);
+
         add(home);
+
+        JLabel text = new JLabel("About us!");
+        text.setBounds(100, 180, 400, 100);
+        text.setForeground(commonconstant.TEXT_COLOR.brighter());
+        text.setFont(new Font("Dialog", Font.BOLD, 50));
+        text.setHorizontalAlignment(SwingConstants.CENTER);
+        add(text);
+
+        JLabel comment = new JLabel("Welcome to MEDCARE Medical Appointment! We are dedicated to revolutionizing the way you manage");
+        comment.setBounds(150, 200, 600, 150);
+        comment.setForeground(commonconstant.TEXT_COLOR.brighter());
+        comment.setFont(new Font("Dialog", Font.BOLD, 12));
+        comment.setHorizontalAlignment(SwingConstants.CENTER);
+        add(comment);
+
+        JLabel comment1 = new JLabel("your healthcare appointments. Our platform is designed to provide seamless scheduling, ensuring you");
+        comment1.setBounds(153, 220,600 , 150);
+        comment1.setForeground(commonconstant.TEXT_COLOR.brighter());
+        comment1.setFont(new Font("Dialog", Font.BOLD, 12));
+        comment1.setHorizontalAlignment(SwingConstants.CENTER);
+        add(comment1);
+
+        JLabel comment2 = new JLabel("can access medical care conveniently and efficiently.");
+        comment2.setBounds(115, 240, 400, 150);
+        comment2.setForeground(commonconstant.TEXT_COLOR.brighter());
+        comment2.setFont(new Font("Dialog", Font.BOLD, 12));
+        comment2.setHorizontalAlignment(SwingConstants.CENTER);
+        add(comment2);
+
+        JLabel text1 = new JLabel("Our Mission");
+        text1.setBounds(100, 310, 400, 100);
+        text1.setForeground(commonconstant.TEXT_COLOR.brighter());
+        text1.setFont(new Font("Dialog", Font.BOLD, 40));
+        text1.setHorizontalAlignment(SwingConstants.CENTER);
+        add(text1);
+
+        JLabel comment3 = new JLabel("At [Medical Appointment Booking Service], our mission is to empower individuals to take control");
+        comment3.setBounds(130, 320, 600, 150);
+        comment3.setForeground(commonconstant.TEXT_COLOR.brighter());
+        comment3.setFont(new Font("Dialog", Font.BOLD, 12));
+        comment3.setHorizontalAlignment(SwingConstants.CENTER);
+        add(comment3);
+
+        JLabel comment4 = new JLabel("of their health by simplifying the appointment booking process. We aim to bridge the gap ");
+        comment4.setBounds(130, 340, 600, 150);
+        comment4.setForeground(commonconstant.TEXT_COLOR.brighter());
+        comment4.setFont(new Font("Dialog", Font.BOLD, 12));
+        comment4.setHorizontalAlignment(SwingConstants.CENTER);
+        add(comment4);
     }
 }
